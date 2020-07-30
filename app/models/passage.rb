e715871a693c1a8a7f721b9b2b91775406290689
github username <github email address>
2020-07-30 17:34:37 -0400

    POST route for passages

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,4 +1,6 @@
 class Passage < ApplicationRecord
   has_many :games
   has_many :users, through: :games
+
+  validates :text, presence: true
 end
--- db/migrate/20200723224134_create_passages.rb
+++ db/migrate/20200723224134_create_passages.rb
@@ -3,8 +3,8 @@ class CreatePassages < ActiveRecord::Migration[6.0]
     create_table :passages do |t|
       t.text :text
       t.string :language
-      t.integer :skill_level
-      t.integer :time_allotted
+      t.integer :skill_level, default: 2
+      t.integer :time_allotted, default: 60
 
       t.timestamps
     end
